1100fba8d25c4be7c1de37e1d59aaa7a40b659bb
xiao <yuxiao08322@gmail.com>
2020-08-18 15:06:36 -0700

    skeleton

---
+++ frontend/pinshare.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+
+    // testing
+
+    ReactDOM.render(<h1>Welcome to Pinshare</h1>, root);
+});
